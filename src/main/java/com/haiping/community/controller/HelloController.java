02cbf636108da2b0fddc62111c563eb94ca10d6c
tom_glb <goodMorning_glb@qq.com>
2021-08-21 11:48:21 +0800

    init repo

---
+++ src/main/java/com/haiping/community/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.haiping.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author kakarotto
+ * @create 2021-08-21-10:44
+ * @Description:
+ * @Modified By:
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
---
+++ src/test/java/com/haiping/community/ComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiping.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
